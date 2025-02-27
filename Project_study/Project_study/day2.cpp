b9e935a21d0dbbcbb694c7c49e73aa98a4c473ff
Songsungsang <mrcomsu@gmail.com>
2025-02-27 12:30:01 +0900

    [Add] Day2 practice, basic I/O

---
+++ Project_study/Project_study/day2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int day2() { 
+	// f5 - 디버깅을 하면서 실행
+	// ctrl + f5 - 디버깅 없이 실행
+	// shift+f5 - 디버깅 종료
+	cout << "을밀대 촬영 궁금해요\n";
+	cout << "을밀대 촬영 궁금해요" << endl; // endl = \n
+
+	string s = "Hello World";
+
+	int myInput;
+	cin >> myInput;
+	cout << myInput;
+
+	return 0;
+}
---
+++ Project_study/Project_study/day2_practice1.cpp
@@ -0,0 +1,20 @@
+﻿//실습1.변수와 자료형
+//▪“이름을 입력하세요.” 라는 문구를 출력하고 사용자로부터 이름을 입력 받기
+//▪“나이를 입력하세요.” 라는 문구를 출력하고 사용자로부터 나이를 입력 받기
+//▪이름과 나이의 입력이 끝나면 "안녕하세요! OOO님(O세)" 라는 문구 출력
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+	int age;
+
+	cout << "이름을 입력하세요. : ";
+	cin >> name;
+	cout << "나이를 입력하세요. : ";
+	cin >> age;
+	
+	cout << "안녕하세요! " << name << "님(" << age << "세)" << endl;
+
+	return 0;
+}
