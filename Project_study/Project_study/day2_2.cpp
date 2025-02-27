64070c7d73e49a5447be8fa225728ad38fcb0f9f
Songsungsang <mrcomsu@gmail.com>
2025-02-27 13:18:58 +0900

    [Add] Adding for new study

---
+++ Project_study/Project_study/day2_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int day2() {
+	int a = 10;
+	a = a + 5;
+	a += 5;
+
+	//증감연산자
+	//값을 한단계 증가하거나 감소
+	int b = 0;
+	b++; // 전치(위). 담고 더해요
+	++b; // 후치(위). 더하고 담아요
+	cout << b;
+
+	return 0;
+}
---
+++ Project_study/Project_study/day2_bit_calculation.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+	// & (and) 비트 연산에서는 & 한개만 사용
+
+	return 0;
+}
