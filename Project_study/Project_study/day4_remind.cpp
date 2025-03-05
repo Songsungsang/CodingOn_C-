52d392ec0f4b3be2b81db915c4befe29f5eaea69
Songsungsang <mrcomsu@gmail.com>
2025-03-05 10:09:15 +0900

    [Add] Revision on functions

---
+++ Project_study/Project_study/day4_remind.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int day4_revise() {
+	int i;
+	// for문 - 횟수가 정해져 있을때
+	for (i = 0; i < 5; i++) {
+		cout << 1;
+	}
+	// 배열의 요소 하나하나마다 보기
+	int numbers[4] = {10, 20, 30, 40};
+	for (int number : numbers) {
+		cout << number << endl;
+	}
+
+	// while문 - 횟수 외의 다른 조건에 따라 반복할때
+	while (true) {
+		//조건을 만족하면
+		break;
+	}
+
+}
