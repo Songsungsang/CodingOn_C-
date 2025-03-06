454ef552c1f82342f4e1e072fe9b0d005f959b21
Songsungsang <mrcomsu@gmail.com>
2025-03-06 11:45:36 +0900

    [Update] Practice of recursive function fibonacci number

--- Project_study/Project_study/day5_practices_function.cpp
+++ Project_study/Project_study/day5_practices_function.cpp
@@ -29,6 +29,8 @@ int findBiggestThree(int x, int y, int z);
 int factorial(int n);
 int factorialRe(int n);
 int powerMultiplyRe(int a, int b);
+int fibonacci(int a, int b, int c, int n);
+int fibo_answer(int n);
 
 int practice_functions() {
 	//실습1.사칙연산 함수 만들기
@@ -73,16 +75,40 @@ int practice_functions() {
 	//int result = factorialRe(5);
 	//cout << "팩토리얼: " << result << endl;
 
+	// 실습5. 피보나치 수열(Fibonacci Numbers)
+	//1.먼저 반복문을 활용해서피보나치 수열을 구현합니다.
+	//2.1번을 바탕으로 작동 원리를 파악하고, 재귀함수를 이용해서 피보나치 수열을 구현합니다.
+	//3.디버거를 이용해 재귀함수의 작동을 확인합니다.
+	int a = 0, b = 1, c, n = 6, i;
+	for (i = 1; i < n; i++) {
+		if (n <= 0) break;
+		c = a + b;
+		cout << i << "번째 숫자 모음 -> " << a << ", " << b << ", " << c << endl;
+		a = b;
+		b = c;
+	}
+
+	cout << n << "번째 피보나치 수열 값: " << c << endl;
+
+	int number, number2, n2 = 5;
+	a = 0, b = 1, c = 0;
+	number2 = fibonacci(a, b, c, n2);
+	cout << n2 + 1 <<  "번째 재귀함수 피보나치 값: " << number2 << endl;
+
+	number = fibo_answer(n);
+	cout << n << "번째 재귀함수 피보나치 값: " << number << endl;
+
 	//실습6.거듭 제곱
 	//자연수 a와 b가 주어졌을 때, a의 b제곱을 계산하는 재귀 함수를 만들어 봅시다.
 	//▪거듭 제곱은 다음과 같이 정의됩니다 :
 	//a^b = a x a^(b-1)
 
-	int a = 2, b = 6;
-	int result = 0;
+	//int a = 2, b = 6;
+	//int result = 0;
 
-	result = powerMultiplyRe(a, b);
-	cout << "거듭 제곱 결과: " << result << endl;
+	//result = powerMultiplyRe(a, b);
+	//cout << "거듭 제곱 결과: " << result << endl;
+
 	return 0;
 }
 
@@ -119,6 +145,22 @@ int factorialRe(int n) {
 	if (n == 1) return 1;
 	int factValue = factorialRe(n-1);
 	return n * factValue;
+}
+
+int fibonacci(int a, int b, int c, int n) { // 시작 a = 1, b = 1, c = 0
+	if (n <= 0) return 0;
+	if (n == 1) return 1;
+	if (n == 2) return 2;
+	c = a + b;
+	a = b;
+	b = c;
+	return c + fibonacci(a, b, c, n-1);
+}
+
+int fibo_answer(int n) { // 재귀함수를 노리는 실제 정답
+	if (n <= 0) return 0;
+	if (n <= 2) return 1;
+	return fibo_answer(n - 1) + fibo_answer(n - 2); // 아 n-1과 n-2를 계속 더해서 쌓아나가는 구나
 }
 
 int powerMultiplyRe(int a, int b) {
